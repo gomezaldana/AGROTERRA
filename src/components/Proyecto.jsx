ba75fc038cdec69256b9a5e1911b029bd084e693
gomezaldana <aldana0gomez@gmail.com>
2024-11-07 22:04:17 -0300

    Implemento funcionalidad para poder donar a un proyecto

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -1,33 +1,35 @@
+
 import React, { useEffect, useState, useRef } from 'react';
 import "../App.css";
+import PagoButton from './PagoButton';
 
 export const Proyecto = () => {
     const [proyecto, setProyecto] = useState(null);
     const [autorProyecto, setAutorProyecto] = useState(null);
 
+
     useEffect(() => {
         fetchProyecto();
-    }, []); // Solo se ejecuta una vez cuando el componente se monta
+    }, []); 
 
     // Función para obtener los datos del proyecto
     const fetchProyecto = async () => {
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2NzJhNmIyZTY0YzY0ZTRmM2U2ZTAyNzgiLCJuYW1lIjoiQW50byIsImlhdCI6MTczMTAwODIxMCwiZXhwIjoxNzMxMDIyNjEwfQ.70Jg56ihPGBHjNMouFAalmPhTLmk8ri8BVMzrkQGpyY"
-        const idProject = "672d33d8b3f78e8ca5316ef5";
+        // Cuando se logueen, guardar token y buscarlo desde ahi
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2NzJkMjJiNTI2MDQ0ZTdlZjRkYjEwZjYiLCJuYW1lIjoiQWxmcmVkbyIsImlhdCI6MTczMTAyNTk3MiwiZXhwIjoxNzMxMDQwMzcyfQ.VG_c2tqex3a2Lw25hpo6xlHDyDmAQGn_HPZabUDnsRw"
 
+        //Cuando alguien apriete ver en detalle un proyecto, poner id aca
+        const idProject = "672d33d8b3f78e8ca5316ef5";
         const response = await fetch(`http://localhost:8080/api/projects/${idProject}`, {
             method: 'GET',
             headers: {
                 'x-token': token,
             },
         });
-
         if (!response.ok) {
             throw new Error(`Error al intentar ver el proyecto [${idProject}]`);
         }
-
         const data = await response.json();
         setProyecto(data);
-
         fetchAutorProyecto(data.userId);
     };
 
@@ -36,26 +38,20 @@ export const Proyecto = () => {
         const response = await fetch(`http://localhost:8080/api/auth/user/${idUser}`, {
             method: 'GET'
         });
-
         if (!response.ok) {
             throw new Error(`Error al obtener al autor del proyecto [${idUser}]`);
         }
-
         const data = await response.json();
         setAutorProyecto(data);
     };
-
 
     return (
         <div className='container mt-5'>
             <div className="row">
                 <p>s</p>
-
                 {proyecto && autorProyecto ? (<>
-
                     <div className="col">
                         {proyecto.images && proyecto.images.length > 0 && (
-
                             <img
                                 className='rounded border mt-2 '
                                 src={`data:${proyecto.images[0].data.contentType};base64,${proyecto.images[0].data}`} // saco la imagen guardada en la bd
@@ -66,13 +62,9 @@ export const Proyecto = () => {
                     </div>
                     <div className="col ">
                         <h1 className='mt-1 text-center mb-4 text-uppercase'>{proyecto.title}</h1>
-
                         <p className="fs-5" >{autorProyecto.name} {autorProyecto.lastName} es la persona que organiza esta recaudación de fondos.</p>
-
                         <p>{proyecto.resume}</p>
-
-                        {/*  <p className="fs-6 fw-bolder mb-2 text-success-emphasis text-start">CONTRIBUIDO {proyecto.raised}</p>
-                        <p style={{ fontSize: "1rem" }} className="fw-bolder mb-2 text-success-emphasis text-end">OBJETIVO {proyecto.amount}</p> */}
+                       
                         <div className="d-flex justify-content-between">
                             <p className="fs-6 fw-bolder mb-2 text-success-emphasis text-start">
                                 CONTRIBUIDO ${proyecto.raised}
@@ -81,31 +73,23 @@ export const Proyecto = () => {
                                 OBJETIVO ${proyecto.amount}
                             </p>
                         </div>
-
-
                         <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                             <div className="progress-bar bg-success" style={{ width: '25%' }}></div>
                         </div>
 
-                        <button className='btn btn-outline-success mt-4 mb-5 '>APOYAR</button>
+                        <PagoButton  proyecto={proyecto}></PagoButton>
+                        
                     </div>
-
                     <hr />
-
                     <h2>OBJETIVO</h2>
                     <p className='fs-5 '>{proyecto.description}</p>
-
                     <img
                         className='mt-2 mx-auto d-block'
-
                         src={`data:${proyecto.images[1].data.contentType};base64,${proyecto.images[1].data}`}
                         alt={proyecto.title}
-                        style={{ width: '100%', height: '600px', width: 'auto' }}
+                        style={{ height: '600px', width: 'auto' }}
                     />
-
-
                 </>
-
                 ) : (
                     <p>No se encontró el proyecto.</p>
                 )}
